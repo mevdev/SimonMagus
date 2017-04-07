59c9d850a86bf1b306daac6be47eaed1c9958faa
Robert Linnemann <rlinnema@steelcase.com>
2017-04-07 15:02:32 -0700

    getting started

---
+++ README.md
@@ -0,0 +1,5 @@
+*Simon for QT*
+
+A C++ test app to separate game logic and use QT.
+
+Robert Linnemann
--- game.cpp
+++ game.cpp
@@ -1,9 +1,5 @@
 #include "game.h"
 
-enum SimonColor { red = 0, yellow = 1, green = 2, orange = 3 };
-
-enum GameMode { stopped, simonSaying, userReply, lost };
-
 Game::Game()
 {
 
