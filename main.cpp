a4a9d38505f45fb33062bcafed696540096d813e
Robert Linnemann <rlinnema@steelcase.com>
2017-05-03 17:02:18 -0700

    autolayouts correct now

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    //w.setWindowFlags(Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint);
-    //w.showFullScreen();
+    w.setWindowFlags(Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint);
+    w.showFullScreen();
 
     return a.exec();
 }
